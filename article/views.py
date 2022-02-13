2588f46e2596b626222c1662ae748596772cdcb8
rumeysametin <rumeysametin1166@gmail.com>
2022-02-13 22:19:06 +0300

    save articles

--- article/views.py
+++ article/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import ArticleForm
-
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -14,5 +14,13 @@ def dashboard(request):
     return render(request, "dashboard.html")
 
 def addarticle(request):
-    form = ArticleForm()
+    form = ArticleForm(request.POST or None)
+    if form.is_valid():
+        article = form.save(commit=False)
+        article.author = request.user
+        article.save()
+        
+        messages.success(request, "You have successfully created an article")
+        return redirect("index")
+        
     return render(request, "addarticle.html", {"form":form})
